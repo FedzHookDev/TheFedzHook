7f65f7df7bd926ded66d00f340918c94dcc92926
daniel assa <assa.daniel@gmail.com>
2025-02-12 16:09:50 +0200

    pool wrappers added

---
+++ src/FedzModifyLiquidityWrapper.sol
@@ -0,0 +1,45 @@
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {Currency} from "v4-core/src/types/Currency.sol";
+import {AbsFedzPoolWrapper} from "./AbsFedzPoolWrapper.sol";
+import {IFedzModifyLiquidityWrapper} from "./interfaces/IFedzModifyLiquidityWrapper.sol";
+
+contract FedzModifyLiquidityWrapper is IFedzModifyLiquidityWrapper, AbsFedzPoolWrapper {
+
+
+    constructor(address _TheFedzHook, uint24 _swapFee, int24 tickSpacing, address _poolManager) AbsFedzPoolWrapper(_TheFedzHook, _swapFee, tickSpacing, _poolManager) {}
+
+    function modifiyLiquidity(FedzModifyLiquidityParams memory params) external returns(BalanceDelta delta, BalanceDelta feeDelta) {
+        bytes memory callbackData = abi.encode(msg.sender, params);
+        bytes memory results = IPoolManager(address(poolManager)).unlock(callbackData);
+        return abi.decode(results, (BalanceDelta, BalanceDelta));
+    }
+
+    function unlockCallback(bytes memory data) external returns(bytes memory results) {
+
+        (address player
+        , FedzModifyLiquidityParams memory params
+        ) = abi.decode(data, (address, FedzModifyLiquidityParams));
+
+        PoolKey memory pool = PoolKey({
+            currency0: Currency.wrap(params.token0),
+            currency1: Currency.wrap(params.token1),
+            fee: swapFee,
+            tickSpacing: tickSpacing,
+            hooks: IHooks(TheFedzHook)
+        });
+
+        IPoolManager.ModifyLiquidityParams memory modifyLiquidityParams = IPoolManager.ModifyLiquidityParams({
+            tickLower: params.tickLower,
+            tickUpper: params.tickUpper,
+            liquidityDelta: params.liquidityDelta,
+            salt: bytes32(uint256(uint160(player)))
+        });
+
+        (BalanceDelta delta, BalanceDelta feeDelta) = IPoolManager(poolManager).modifyLiquidity(pool, modifyLiquidityParams, data);
+        results = abi.encode(delta, feeDelta);
+        _closeDelta(player, params.token0, params.token1, delta, pool);
+    }
+}
---
+++ src/FedzSwapWrapper.sol
@@ -0,0 +1,38 @@
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {Currency} from "v4-core/src/types/Currency.sol";
+import {AbsFedzPoolWrapper} from "./AbsFedzPoolWrapper.sol";
+import {IFedzSwapWrapper} from "./interfaces/IFedzSwapWrapper.sol";
+
+contract FedzSwapWrapper is IFedzSwapWrapper, AbsFedzPoolWrapper {
+
+    constructor(address _TheFedzHook, uint24 _swapFee, int24 tickSpacing, address _poolManager) AbsFedzPoolWrapper(_TheFedzHook, _swapFee, tickSpacing, _poolManager) {}
+
+    function swap(address token0, address token1, IPoolManager.SwapParams memory params) external returns(BalanceDelta delta) {
+        bytes memory callbackData = abi.encode(msg.sender, token0, token1, params);
+        bytes memory results = IPoolManager(address(poolManager)).unlock(callbackData);
+        delta = abi.decode(results, (BalanceDelta));
+    }
+
+    function unlockCallback(bytes memory data) public returns(bytes memory results) {
+        (address player
+        , address token0
+        , address token1
+        , IPoolManager.SwapParams memory params
+        ) = abi.decode(data, (address, address, address, IPoolManager.SwapParams));
+
+        PoolKey memory pool = PoolKey({
+            currency0: Currency.wrap(token0),
+            currency1: Currency.wrap(token1),
+            fee: swapFee,
+            tickSpacing: tickSpacing,
+            hooks: IHooks(TheFedzHook)
+        });
+
+        BalanceDelta delta = IPoolManager(poolManager).swap(pool, params, data);
+        results = abi.encode(delta);
+        _closeDelta(player, token0, token1, delta, pool);
+    }
+}
